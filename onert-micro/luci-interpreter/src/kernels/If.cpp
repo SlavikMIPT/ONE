1e8c5a552c9b69165c4112f3c65fb6fc57b14d29
Vyacheslav Bazhenov <v.bazhenov@partner.samsung.com>
2023-11-08 14:29:55 +0300

    [onert-micro] Introduce If kernel
    This commit introduces If kernel.
    
    ONE-DCO-1.0-Signed-off-by: Vyacheslav Bazhenov <slavikmipt@gmail.com>

--- onert-micro/luci-interpreter/src/kernels/If.cpp
+++ onert-micro/luci-interpreter/src/kernels/If.cpp
@@ -23,6 +23,8 @@ namespace luci_interpreter
 
 void configure_kernel_CircleIf(const circle::Operator *cur_op, BaseRuntimeGraph *runtime_graph)
 {
+  auto *main_runtime_graph = runtime_graph;
+
   auto *runtime_module = runtime_graph->getRuntimeModule();
 
   const auto *options = cur_op->builtin_options_as_IfOptions();
@@ -48,21 +50,33 @@ void configure_kernel_CircleIf(const circle::Operator *cur_op, BaseRuntimeGraph 
   auto *else_subgraph = runtime_module->getRuntimeGraphAt(else_subgraph_index);
   for (RuntimeGraph *graph : {then_subgraph, else_subgraph})
   {
-    (void)graph;
-    LUCI_INTERPRETER_CHECK(graph->getNumOfInputTensors() ==
-                           runtime_graph->getNumOfInputTensors() - 1);
-    LUCI_INTERPRETER_CHECK(graph->getNumOfOutputTensors() ==
-                           runtime_graph->getNumOfOutputTensors());
+    graph->selectOwnSubgraph();
+    const auto graph_input_size = graph->getNumOfInputTensors();
+    const auto graph_output_size = graph->getNumOfOutputTensors();
+    LUCI_INTERPRETER_CHECK(graph_input_size == cur_op->inputs()->size() - 1);
+    LUCI_INTERPRETER_CHECK(graph_output_size == cur_op->outputs()->size());
+    graph->invalidate();
+    graph->configure(false);
   }
+  main_runtime_graph->selectOwnSubgraph();
 }
 
 void execute_kernel_CircleIf(const circle::Operator *cur_op, BaseRuntimeGraph *runtime_graph)
 {
+  auto *main_runtime_graph = runtime_graph;
   auto *runtime_module = runtime_graph->getRuntimeModule();
+
+  const auto input_size = cur_op->inputs()->size() - 1;
+  const auto output_size = cur_op->outputs()->size();
+
+  std::vector<uint8_t *> operation_inputs_data(input_size);
+  std::vector<uint8_t *> operation_outputs_data(output_size);
+
+  std::vector<int32_t> input_sizes(input_size);
+  std::vector<int32_t> output_sizes(output_size);
 
   const auto *options = cur_op->builtin_options_as_IfOptions();
   const auto cond_index = cur_op->inputs()->operator[](0);
-  const auto output_index = cur_op->outputs()->operator[](0);
 
   const auto then_subgraph_index = options->then_subgraph_index();
   const auto else_subgraph_index = options->else_subgraph_index();
@@ -71,55 +85,63 @@ void execute_kernel_CircleIf(const circle::Operator *cur_op, BaseRuntimeGraph *r
   auto *else_subgraph = runtime_module->getRuntimeGraphAt(else_subgraph_index);
 
   const auto cond = runtime_graph->getCircleTensorByIndex(cond_index);
-  const auto output = runtime_graph->getCircleTensorByIndex(output_index);
 
   const uint8_t *cond_data = runtime_graph->getDataByTensor(cond);
   const bool cond_value = kernels::getTensorData<bool>(cond_data)[0];
 
   RuntimeGraph *active_graph = cond_value ? then_subgraph : else_subgraph;
 
-  // Copy kernel inputs to active graph inputs.
-  for (size_t i = 0; i < runtime_graph->getNumOfInputTensors() - 1; ++i)
+  for (int32_t i = 0; i < input_size; ++i)
   {
-    auto active_input = active_graph->getInputTensorByIndex(i);
-    auto runtime_input = runtime_graph->getInputTensorByIndex(i);
+    const auto op_input_index = cur_op->inputs()->operator[](i + 1);
+    assert(op_input_index != -1);
+    const auto input = main_runtime_graph->getCircleTensorByIndex(op_input_index);
+    input_sizes[i] = Tensor::num_elements(input) * size(Tensor::element_type(input));
 
-    LUCI_INTERPRETER_CHECK(Tensor::element_type(active_input) ==
-                           (Tensor::element_type(runtime_input)));
+    auto *input_data = main_runtime_graph->getDataByTensor(input);
 
-    //    active_graph->getInputTensorByIndex(i)->resize(
-    //      runtime_graph->getInputTensorByIndex(i)->shape());
+    uint8_t *tensor_data = nullptr;
+    if (input_data == nullptr)
+      input_data = main_runtime_graph->getConstDataByTensor(input);
+    assert(input_data != nullptr);
+    tensor_data = main_runtime_graph->getDataByTensor(input);
+    assert(tensor_data != nullptr);
 
-    const int32_t num_elements = Tensor::num_elements(runtime_input);
-    const std::size_t element_size = size(Tensor::element_type(runtime_input));
-    //    // TODO: Think about how allocate memory for output in main graph
-    //    active_graph->configureAllocations(active_input);
-
-    std::memcpy(runtime_graph->getDataByTensor(active_input),
-                runtime_graph->getDataByTensor(runtime_input), num_elements * element_size);
+    operation_inputs_data[i] = tensor_data;
   }
+  for (int32_t i = 0; i < output_size; ++i)
+  {
+    const auto op_output_index = cur_op->outputs()->operator[](i);
+    assert(op_output_index != -1);
+    const auto output = main_runtime_graph->getCircleTensorByIndex(op_output_index);
+    output_sizes[i] = Tensor::num_elements(output) * size(Tensor::element_type(output));
+
+    auto *output_data = main_runtime_graph->getDataByTensor(output);
+
+    uint8_t *tensor_data = nullptr;
+    if (output_data == nullptr)
+      output_data = main_runtime_graph->getConstDataByTensor(output);
+    assert(output_data != nullptr);
+    tensor_data = main_runtime_graph->getDataByTensor(output);
+    assert(tensor_data != nullptr);
+
+    operation_outputs_data[i] = tensor_data;
+  }
+  active_graph->selectOwnSubgraph();
+  for (int32_t i = 0; i < input_size; ++i)
+    active_graph->configureGraphInput(i, operation_inputs_data[i]);
   active_graph->execute();
 
-  // Copy graph outputs to kernel outputs.
-  for (size_t i = 0; i < runtime_graph->getNumOfOutputTensors(); ++i)
+  for (int32_t i = 0; i < output_size; ++i)
   {
-    auto active_output = active_graph->getOutputTensorByIndex(i);
-    auto runtime_output = runtime_graph->getOutputTensorByIndex(i);
-
-    LUCI_INTERPRETER_CHECK(Tensor::element_type(active_output) ==
-                           (Tensor::element_type(runtime_output)));
-
-    //    active_graph->getInputTensorByIndex(i)->resize(
-    //      runtime_graph->getInputTensorByIndex(i)->shape());
-
-    const int32_t num_elements = Tensor::num_elements(runtime_output);
-    const std::size_t element_size = size(Tensor::element_type(runtime_output));
-    //    // TODO: Think about how allocate memory for output in main graph
-    //    active_graph->configureAllocations(active_input);
-
-    std::memcpy(runtime_graph->getDataByTensor(runtime_output),
-                runtime_graph->getDataByTensor(active_output), num_elements * element_size);
+    auto cur_output_active_data = active_graph->getOutputDataByIndex(i);
+    if (cur_output_active_data == nullptr)
+      continue;
+    std::memcpy(operation_outputs_data[i], cur_output_active_data, output_sizes[i]);
   }
+  active_graph->resetOutputTensorsData();
+  active_graph->clearTensors();
+  main_runtime_graph->selectOwnSubgraph();
 }
 
 } // namespace luci_interpreter
--- onert-micro/luci-interpreter/src/kernels/If.test.cpp
+++ onert-micro/luci-interpreter/src/kernels/If.test.cpp
@@ -1,161 +1,98 @@
-///*
-// * Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
-// * Copyright 2019 The TensorFlow Authors. All Rights Reserved.
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// *    http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//
-//#include "core/RuntimeModule.h"
-//#include "kernels/Add.h"
-//#include "kernels/If.h"
-//#include "kernels/Mul.h"
-//#include "kernels/TestUtils.h"
-//
-//#include "luci_interpreter/TestMemoryManager.h"
-//
-//namespace luci_interpreter
-//{
-//namespace kernels
-//{
-//namespace
-//{
-//
-//using namespace testing;
-//
-//class IfTest : public ::testing::Test
-//{
-//protected:
-//  void SetUp() override { _memory_manager = std::make_unique<TestMemoryManager>(); }
-//
-//  std::unique_ptr<IMemoryManager> _memory_manager;
-//};
-//
-//RuntimeGraph *buildAddSubgraph(RuntimeModule *module, IMemoryManager *memory_manager)
-//{
-//  RuntimeGraph *graph = module->addGraph(memory_manager);
-//  Tensor *input1 = graph->addTensor(
-//    std::make_unique<Tensor>(DataType::FLOAT32, Shape{}, AffineQuantization{}, ""));
-//  Tensor *input2 = graph->addTensor(
-//    std::make_unique<Tensor>(DataType::FLOAT32, Shape{}, AffineQuantization{}, ""));
-//  Tensor *output = graph->addTensor(
-//    std::make_unique<Tensor>(DataType::FLOAT32, Shape{}, AffineQuantization{}, ""));
-//
-//  memory_manager->allocate_memory(*input1);
-//  memory_manager->allocate_memory(*input2);
-//  memory_manager->allocate_memory(*output);
-//
-//  graph->setInputTensors({input1, input2});
-//  graph->setOutputTensors({output});
-//
-//  AddParams params{};
-//  params.activation = Activation::NONE;
-//  graph->addKernel(std::make_unique<Add>(input1, input2, output, params));
-//
-//  return graph;
-//}
-//
-//RuntimeGraph *buildMulSubgraph(RuntimeModule *module, IMemoryManager *memory_manager)
-//{
-//  RuntimeGraph *graph = module->addGraph(memory_manager);
-//  Tensor *input1 = graph->addTensor(
-//    std::make_unique<Tensor>(DataType::FLOAT32, Shape{}, AffineQuantization{}, ""));
-//  Tensor *input2 = graph->addTensor(
-//    std::make_unique<Tensor>(DataType::FLOAT32, Shape{}, AffineQuantization{}, ""));
-//  Tensor *output = graph->addTensor(
-//    std::make_unique<Tensor>(DataType::FLOAT32, Shape{}, AffineQuantization{}, ""));
-//
-//  memory_manager->allocate_memory(*input1);
-//  memory_manager->allocate_memory(*input2);
-//  memory_manager->allocate_memory(*output);
-//
-//  graph->setInputTensors({input1, input2});
-//  graph->setOutputTensors({output});
-//
-//  MulParams params{};
-//  params.activation = Activation::NONE;
-//  graph->addKernel(std::make_unique<Mul>(input1, input2, output, params));
-//
-//  return graph;
-//}
-//
-//TEST_F(IfTest, CondTrue)
-//{
-//  Tensor cond = makeInputTensor<DataType::BOOL>({1}, {true}, _memory_manager.get());
-//  Tensor input1 = makeInputTensor<DataType::FLOAT32>({2}, {5, 7}, _memory_manager.get());
-//  Tensor input2 = makeInputTensor<DataType::FLOAT32>({1, 2}, {1, 2}, _memory_manager.get());
-//  Tensor output = makeOutputTensor(DataType::FLOAT32);
-//
-//  RuntimeModule module(nullptr);
-//  RuntimeGraph *then_graph = buildAddSubgraph(&module, _memory_manager.get());
-//  RuntimeGraph *else_graph = buildMulSubgraph(&module, _memory_manager.get());
-//
-//  If kernel(&cond, {&input1, &input2}, {&output}, then_graph, else_graph);
-//  kernel.configure();
-//  _memory_manager->allocate_memory(output);
-//  kernel.execute();
-//
-//  EXPECT_THAT(extractTensorData<float>(output), FloatArrayNear({6, 9}));
-//}
-//
-//TEST_F(IfTest, CondFalse)
-//{
-//  Tensor cond = makeInputTensor<DataType::BOOL>({1}, {false}, _memory_manager.get());
-//  Tensor input1 = makeInputTensor<DataType::FLOAT32>({2}, {5, 7}, _memory_manager.get());
-//  Tensor input2 = makeInputTensor<DataType::FLOAT32>({1, 2}, {1, 2}, _memory_manager.get());
-//  Tensor output = makeOutputTensor(DataType::FLOAT32);
-//
-//  RuntimeModule module(nullptr);
-//  RuntimeGraph *then_graph = buildAddSubgraph(&module, _memory_manager.get());
-//  RuntimeGraph *else_graph = buildMulSubgraph(&module, _memory_manager.get());
-//
-//  If kernel(&cond, {&input1, &input2}, {&output}, then_graph, else_graph);
-//  kernel.configure();
-//  _memory_manager->allocate_memory(output);
-//  kernel.execute();
-//
-//  EXPECT_THAT(extractTensorData<float>(output), FloatArrayNear({5, 14}));
-//}
-//
-//TEST_F(IfTest, InvalidCondType_NEG)
-//{
-//  Tensor cond = makeInputTensor<DataType::FLOAT32>({1}, {1}, _memory_manager.get());
-//  Tensor input1 = makeInputTensor<DataType::FLOAT32>({2}, {5, 7}, _memory_manager.get());
-//  Tensor input2 = makeInputTensor<DataType::FLOAT32>({1, 2}, {1, 2}, _memory_manager.get());
-//  Tensor output = makeOutputTensor(DataType::FLOAT32);
-//
-//  RuntimeModule module(nullptr);
-//  RuntimeGraph *then_graph = buildAddSubgraph(&module, _memory_manager.get());
-//  RuntimeGraph *else_graph = buildMulSubgraph(&module, _memory_manager.get());
-//
-//  If kernel(&cond, {&input1, &input2}, {&output}, then_graph, else_graph);
-//  EXPECT_ANY_THROW(kernel.configure());
-//}
-//
-//TEST_F(IfTest, InvalidCondElementNum_NEG)
-//{
-//  Tensor cond = makeInputTensor<DataType::BOOL>({2}, {false, true}, _memory_manager.get());
-//  Tensor input1 = makeInputTensor<DataType::FLOAT32>({2}, {5, 7}, _memory_manager.get());
-//  Tensor input2 = makeInputTensor<DataType::FLOAT32>({1, 2}, {1, 2}, _memory_manager.get());
-//  Tensor output = makeOutputTensor(DataType::FLOAT32);
-//
-//  RuntimeModule module(nullptr);
-//  RuntimeGraph *then_graph = buildAddSubgraph(&module, _memory_manager.get());
-//  RuntimeGraph *else_graph = buildMulSubgraph(&module, _memory_manager.get());
-//
-//  If kernel(&cond, {&input1, &input2}, {&output}, then_graph, else_graph);
-//  EXPECT_ANY_THROW(kernel.configure());
-//}
-//
-//} // namespace
-//} // namespace kernels
-//} // namespace luci_interpreter
+/*
+ * Copyright (c) 2021 Samsung Electronics Co., Ltd. All Rights Reserved
+ * Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "kernels/TestUtils.h"
+#include "luci_interpreter/test_models/if/IfKernel.h"
+#include "luci_interpreter/test_models/if/NegIfKernel.h"
+
+#include "loader/ModuleLoader.h"
+
+namespace luci_interpreter
+{
+namespace
+{
+
+using namespace testing;
+
+class IfTest : public ::testing::Test
+{
+  // Do nothing
+};
+
+template <typename T> std::vector<T> checkIfKernel(test_kernel::TestDataBase<T> *test_data_base)
+{
+  MemoryManager memory_manager{};
+  RuntimeModule runtime_module{};
+  bool dealloc_input = true;
+
+  // Load model with single op
+  auto *model_data_raw = reinterpret_cast<const char *>(test_data_base->get_model_ptr());
+  ModuleLoader::load(&runtime_module, &memory_manager, model_data_raw, dealloc_input);
+
+  auto *main_runtime_graph = runtime_module.getMainGraph();
+  assert(main_runtime_graph->getNumOfInputTensors() == 3);
+
+  // Set cond input data
+  {
+    // assume that tensor is bool of size 1
+    bool *input_tensor_data = reinterpret_cast<bool *>(main_runtime_graph->configureGraphInput(0));
+    *input_tensor_data = static_cast<bool>(test_data_base->get_input_data_by_index(0)[0]);
+  }
+  // Set input1 data
+  {
+    auto *input_tensor_data = reinterpret_cast<T *>(main_runtime_graph->configureGraphInput(1));
+    std::copy(test_data_base->get_input_data_by_index(1).begin(),
+              test_data_base->get_input_data_by_index(1).end(), input_tensor_data);
+  }
+  // Set input2 data
+  {
+    auto *input_tensor_data = reinterpret_cast<T *>(main_runtime_graph->configureGraphInput(2));
+    std::copy(test_data_base->get_input_data_by_index(2).begin(),
+              test_data_base->get_input_data_by_index(2).end(), input_tensor_data);
+  }
+  runtime_module.execute();
+
+  assert(main_runtime_graph->getNumOfOutputTensors() == 1);
+
+  T *output_data = reinterpret_cast<T *>(main_runtime_graph->getOutputDataByIndex(0));
+  const size_t num_elements = (main_runtime_graph->getOutputDataSizeByIndex(0) / sizeof(T));
+  std::vector<T> output_data_vector(output_data, output_data + num_elements);
+  return output_data_vector;
+}
+
+TEST_F(IfTest, MainTest_P)
+{
+  test_kernel::TestDataIfKernel<float> test_data_kernel;
+  std::vector<float> output_data_vector = checkIfKernel(&test_data_kernel);
+  EXPECT_THAT(output_data_vector, test_data_kernel.get_output_data_by_index(0));
+}
+
+TEST_F(IfTest, MainTest_NEG)
+{
+  test_kernel::NegTestDataIfKernel test_data_kernel;
+
+  MemoryManager memory_manager{};
+  RuntimeModule runtime_module{};
+  bool dealloc_input = true;
+  // Load model with single op
+  auto *model_data_raw = reinterpret_cast<const char *>(test_data_kernel.get_model_ptr());
+  EXPECT_DEATH(ModuleLoader::load(&runtime_module, &memory_manager, model_data_raw, dealloc_input),
+               "");
+}
+
+} // namespace
+} // namespace luci_interpreter
